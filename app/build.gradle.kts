10a9d5cde24a59fd1eca34d7faa25031158269b0
MajorTanya <github@majortanya.eu>
2024-03-10 18:38:16 +0100

    Bump DummyExtension to 1.4.6
    Update Gradle to 8.6 without AGP.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationIdSuffix = "all.dummyextension"
         minSdk = 26
         targetSdk = 34
-        versionCode = 5
+        versionCode = 6
         versionName = "1.4.$versionCode"
         base {
             archivesName = "tachiyomi-$applicationIdSuffix-v$versionName"
