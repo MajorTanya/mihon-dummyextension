532472ed782470785ca057f8717871cf5b7fb5cd
MajorTanya <github@majortanya.eu>
2024-04-23 01:37:57 +0200

    Bump DummyExtension to 1.4.11
    Insert joke here about _weekly_ updates when 1.4.10 happened on 2024-03-22 and
    this one happened on 2024-04-22.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationIdSuffix = "all.dummyextension"
         minSdk = 26
         targetSdk = 34
-        versionCode = 10
+        versionCode = 11
         versionName = "1.4.$versionCode"
         base {
             archivesName = "tachiyomi-$applicationIdSuffix-v$versionName"
