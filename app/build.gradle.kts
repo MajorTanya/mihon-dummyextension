397cbecfc245d4a85127fd1a0bf7af04b18ebdcb
MajorTanya <github@majortanya.eu>
2024-04-23 02:29:28 +0200

    Bump DummyExtension to 1.4.16
    Bumping just the version to simulate a routine version increase, if this works
    fine we should be good for the foreseeable future.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationIdSuffix = "all.dummyextension"
         minSdk = 26
         targetSdk = 34
-        versionCode = 15
+        versionCode = 16
         versionName = "1.4.$versionCode"
         base {
             archivesName = "tachiyomi-$applicationIdSuffix-v$versionName"
