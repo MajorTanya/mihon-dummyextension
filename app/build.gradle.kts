e6c9907c4e3bf80d9b824c20e513a1798f1d8adf
MajorTanya <39014446+MajorTanya@users.noreply.github.com>
2026-07-19 10:07:10 +0200

    Bump DummyExtension to 1.4.21
    RIP to funny number, I need to test stuff
    
    Also no idea if all this still works

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationIdSuffix = "all.dummyextension"
         minSdk = 26
         targetSdk = 34
-        versionCode = 20
+        versionCode = 21
         versionName = "${libs.versions.extlib.get().substringBeforeLast('.')}.$versionCode"
         base {
             archivesName = "tachiyomi-$applicationIdSuffix-v$versionName"
