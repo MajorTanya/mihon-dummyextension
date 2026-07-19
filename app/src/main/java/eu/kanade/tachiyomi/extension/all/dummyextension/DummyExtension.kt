2652db09f14cbbc6e7acfe381d3b8891c28310b8
MajorTanya <github@majortanya.eu>
2026-07-19 11:13:22 +0200

    Replace methods with only tachiyomix 1.6 methods
    If any non-1.6-supporting apps try to use it (and the extension loader
    magically accepts 1.6 somehow?), the deprecated methods throw "Stub!" anyway,
    so it's fine.

--- app/src/main/java/eu/kanade/tachiyomi/extension/all/dummyextension/DummyExtension.kt
+++ app/src/main/java/eu/kanade/tachiyomi/extension/all/dummyextension/DummyExtension.kt
@@ -2,9 +2,6 @@ package eu.kanade.tachiyomi.extension.all.dummyextension
 
 import eu.kanade.tachiyomi.source.model.FilterList
 import eu.kanade.tachiyomi.source.model.MangasPage
-import eu.kanade.tachiyomi.source.model.Page
-import eu.kanade.tachiyomi.source.model.SChapter
-import eu.kanade.tachiyomi.source.model.SManga
 import eu.kanade.tachiyomi.source.online.HttpSource
 
 @Suppress("unused")
@@ -14,35 +11,12 @@ class DummyExtension : HttpSource() {
     override val name: String = "Dummy Extension"
     override val supportsLatest: Boolean = false
 
-    override fun chapterListParse(response: okhttp3.Response): List<SChapter> =
+    override suspend fun getPopularManga(page: Int): MangasPage =
         throw UnsupportedOperationException("This is a non-functioning Dummy Extension")
 
-    override fun imageUrlParse(response: okhttp3.Response): String =
-        throw UnsupportedOperationException("This is a non-functioning Dummy Extension")
-
-    override fun latestUpdatesParse(response: okhttp3.Response): MangasPage =
-        throw UnsupportedOperationException("This is a non-functioning Dummy Extension")
-
-    override fun latestUpdatesRequest(page: Int): okhttp3.Request =
-        throw UnsupportedOperationException("This is a non-functioning Dummy Extension")
-
-    override fun mangaDetailsParse(response: okhttp3.Response): SManga =
-        throw UnsupportedOperationException("This is a non-functioning Dummy Extension")
-
-    override fun pageListParse(response: okhttp3.Response): List<Page> =
-        throw UnsupportedOperationException("This is a non-functioning Dummy Extension")
-
-    override fun popularMangaParse(response: okhttp3.Response): MangasPage =
-        throw UnsupportedOperationException("This is a non-functioning Dummy Extension")
-
-    override fun popularMangaRequest(page: Int): okhttp3.Request =
-        throw UnsupportedOperationException("This is a non-functioning Dummy Extension")
-
-    override fun searchMangaParse(response: okhttp3.Response): MangasPage =
-        throw UnsupportedOperationException("This is a non-functioning Dummy Extension")
-
-    override fun searchMangaRequest(
-        page: Int, query: String, filters: FilterList
-    ): okhttp3.Request =
-        throw UnsupportedOperationException("This is a non-functioning Dummy Extension")
+    override suspend fun getSearchManga(
+        page: Int,
+        query: String,
+        filters: FilterList
+    ): MangasPage = throw UnsupportedOperationException("This is a non-functioning Dummy Extension")
 }
